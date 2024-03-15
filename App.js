8fece1bca28118e4adfc2f57c93d616d98aadd2b
saiprasanthg <saiprasanth.guthula@sjsu.edu>
2024-03-14 20:48:06 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,75 +1,65 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { useAuth0, Auth0Provider } from 'react-native-auth0';
+import { Auth0Provider } from 'react-native-auth0';
 import { SafeAreaView, StatusBar, StyleSheet, useColorScheme } from 'react-native';
-import LandingPage from './components/LandingPage';
+// Import your components
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import config from './components/auth0-configuration';
+
+// Import screens
+import LandingPage from './components/LandingPage';
 import NewPatientForm from './components/NewPatientForm';
 import AppNavigator from './components/AppNavigator';
 import AccountPage from './components/AccountPage';
-import { useSelector } from 'react-redux';
 import AvailableAppointments from './components/AvailableAppointments';
 import MentalContent from './components/MentalContentPage';
 import PlanPage from './components/PlanPage';
-import AppointmentsPage from './components/AppointmentsPage';
-
 
 const Stack = createStackNavigator();
 
+// Logged-in user navigator
+const LoggedInNavigator = () => (
+  <Stack.Navigator initialRouteName="AppNavigator" screenOptions={{ headerShown: true }}>
+    <Stack.Screen name="AppNavigator" component={AppNavigator} options={{ title: 'My Appointments', headerLeft: null, headerTitleAlign: 'center' }} />
+    <Stack.Screen name="Account" component={AccountPage} />
+    <Stack.Screen name="MentalContent" component={MentalContent} />
+    <Stack.Screen name="PlanPage" component={PlanPage} />
+    <Stack.Screen name="AvailableAppointments" component={AvailableAppointments} />
+    <Stack.Screen name="LandingPage" component={LandingPage} />
+    <Stack.Screen name="NewPatientForm" component={NewPatientForm} />
+  </Stack.Navigator>
+);
+
+// Guest user navigator
+const GuestNavigator = () => (
+  <Stack.Navigator initialRouteName="LandingPage" screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="LandingPage" component={LandingPage} />
+    <Stack.Screen name="NewPatientForm" component={NewPatientForm} screenOptions={{ headerShown: true }} />
+    <Stack.Screen name="AppNavigator" component={AppNavigator} />
+  </Stack.Navigator>
+);
+
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
-  const { user } = useAuth0();
-  const user_state = useSelector(state => state.auth.user)
-  console.log('user state: ',user_state);
-  useEffect(() => {
-    if (user) {
-      console.log('User is logged in, navigating to AppNavigator');
-    } else {
-      console.log('User is not logged in, staying at LandingPage');
-    }
-  }, [user]);
+  const [isLoggedIn, setLoggedIn] = useState(false);
 
-  console.log('user at app:', user);
-  console.log('App component is rendered');
+  async function getData() {
+    const data = await AsyncStorage.getItem('isLoggedIn');
+    console.log('Data at app:', data);
+    setLoggedIn(data === 'true'); // Assuming 'isLoggedIn' is stored as a string
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <Auth0Provider domain={config.domain} clientId={config.clientId}>
       <SafeAreaView style={styles.root}>
         <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="LandingPage" screenOptions={{ headerShown: true }}>
-
-            <Stack.Screen name="AppNavigator" 
-              component={AppNavigator} 
-              options={{
-                title: 'My Appointments',
-                headerLeft: null,
-                headerTitleAlign: 'center'
-              }}/>
-
-            <Stack.Screen name="LandingPage" 
-              component={LandingPage} 
-              screenOptions={{ 
-                headerShown: false 
-              }}/>
-
-            <Stack.Screen name="NewPatientForm" 
-              component={NewPatientForm} 
-              screenOptions={{ headerShown: true }}/>
-
-            <Stack.Screen name="Account" 
-              component={AccountPage} />
-
-            <Stack.Screen name="MentalContent" 
-              component={MentalContent} />
-
-            <Stack.Screen name="PlanPage" 
-              component={PlanPage} />
-
-            <Stack.Screen name="AvailableAppointments" 
-              component={AvailableAppointments} />
-          </Stack.Navigator>
+          {isLoggedIn ? <LoggedInNavigator /> : <GuestNavigator />}
         </NavigationContainer>
       </SafeAreaView>
     </Auth0Provider>
